4d2ed7acd8eac9e78d5b5d0f532fabb4cad48e2a
januszpolak <janusz.polak@interia.pl>
2020-10-22 11:16:55 +0200

    Edit style

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace WeatherWPF
 {
@@ -30,14 +31,20 @@ namespace WeatherWPF
             int pressure = weatherResults.main.pressure;
             double humidity = weatherResults.main.humidity;
             double visibility = weatherResults.visibility;
-            string desc = weatherResults.weather[0].description;
+            string desc = weatherResults.weather[0].main;
 
             TextBlockTemp.Text = temperature.ToString() + "°C";
             TextBlockPressure.Text = "Ciśnienie: " + pressure.ToString() + " hPa";
             TextBlockHumidity.Text = "Wilgotność: " + humidity.ToString() + "%";
             TextBlockVisibility.Text = "Widoczność: " + visibility.ToString() + "m";
 
-            
+
+
+            if (desc == "Clouds")
+            {
+                Image.Source = new BitmapImage(new System.Uri("https://cdn.pixabay.com/photo/2017/02/07/01/59/cloud-2044797_960_720.png"));
+            }
+
         }
 
       
