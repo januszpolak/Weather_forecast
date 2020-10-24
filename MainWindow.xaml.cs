8e112fbb8567bc87228d7fec4c47b17279f165b0
januszpolak <janusz.polak@interia.pl>
2020-10-24 14:22:36 +0200

    add fixed size to main window and set backgrounds depends to weather condition

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace WeatherWPF
@@ -43,30 +45,37 @@ namespace WeatherWPF
             if (desc == "Clouds")
             {
                 Image.Source = new BitmapImage(new System.Uri("https://cdn.pixabay.com/photo/2017/02/07/01/59/cloud-2044797_960_720.png"));
+                GridMain.Background = new ImageBrush(new BitmapImage(new Uri("https://cdn.pixabay.com/photo/2015/07/05/10/18/tree-832079_960_720.jpg")));
             }
             if (desc == "Mist")
             {
                 Image.Source = new BitmapImage(new System.Uri("https://cdn.pixabay.com/photo/2013/04/01/09/21/fog-98505_960_720.png"));
+                GridMain.Background = new ImageBrush(new BitmapImage(new Uri("https://cdn.pixabay.com/photo/2016/02/19/10/22/fog-1209205_960_720.jpg")));
             }
             if (desc == "Clear")
             {
                 Image.Source = new BitmapImage(new System.Uri("https://cdn.pixabay.com/photo/2016/11/18/23/41/sun-1837376_960_720.png"));
+                GridMain.Background = new ImageBrush(new BitmapImage(new Uri("https://cdn.pixabay.com/photo/2014/10/03/16/53/refreshing-471950_960_720.jpg")));
             }
             if (desc == "Snow")
             {
                 Image.Source = new BitmapImage(new System.Uri("https://cdn.pixabay.com/photo/2014/04/02/10/46/snowflake-304521_960_720.png"));
+                GridMain.Background = new ImageBrush(new BitmapImage(new Uri("https://cdn.pixabay.com/photo/2015/02/14/19/46/winter-landscape-636634_960_720.jpg")));
             }
             if (desc == "Rain")
             {
                 Image.Source = new BitmapImage(new System.Uri("https://cdn.pixabay.com/photo/2013/07/13/12/47/drops-160354_960_720.png"));
+                GridMain.Background = new ImageBrush(new BitmapImage(new Uri("https://cdn.pixabay.com/photo/2014/09/21/14/35/surface-455120_960_720.jpg")));
             }
             if (desc == "Drizzle")
             {
                 Image.Source = new BitmapImage(new System.Uri("https://cdn.pixabay.com/photo/2013/07/12/14/49/drops-148870_960_720.png"));
+                GridMain.Background = new ImageBrush(new BitmapImage(new Uri("https://cdn.pixabay.com/photo/2015/08/03/22/25/rain-874041_960_720.jpg")));
             }
             if (desc == "Thunderstorm")
             {
                 Image.Source = new BitmapImage(new System.Uri("https://cdn.pixabay.com/photo/2012/05/07/02/12/thundercloud-47584_960_720.png"));
+                GridMain.Background = new ImageBrush(new BitmapImage(new Uri("https://cdn.pixabay.com/photo/2015/06/08/15/06/lightning-801866_960_720.jpg")));
             }
         }
 
